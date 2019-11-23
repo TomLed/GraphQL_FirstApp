243ae74f3733ca110ed27b644675c27ad7930933
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-23 20:13:09 +0100

    Add the DirectorType

--- schema/schema.js
+++ schema/schema.js
@@ -12,6 +12,12 @@ var films = [
     {title: "Parasite", director: "Bong Joon-ho", id: "3"}
 ];
 
+var directors = [
+    {name:"Gérard Oury", nationality: "Français", id: '1' },
+    {name:"Todd Phillips", nationality: "Américain", id: '2' },
+    {name:"Bong Joon-ho", nationality: "Coréen", id: '3' }
+];
+
 
 // On récupère la fonction GraphQLObjectType
 const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql;
@@ -29,6 +35,19 @@ const FilmType = new GraphQLObjectType({
     })
 });
 
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    /* Il est utile de définir une fonciton plutôt qu'un objet, par exemple dans le cas suivant :
+    When two types need to refer to each other, or a type needs to refer to itself in a field,
+    you can use a function expression (aka a closure or a thunk) to supply the fields lazily.
+     */
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        nationality: {type: GraphQLString}
+    })
+});
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     // On définit ici le nom des requêtes et comment formuler la requête (ici avec un ID) pour obtenir un résultat
@@ -36,9 +55,16 @@ const RootQuery = new GraphQLObjectType({
         film: {
             type: FilmType,
             args: {id: {type: GraphQLID}},
-            resolve(parent,args){
+            resolve(parent, args) {
                 // code pour obtenir les donnée d'une db
                 return _.find(films, {id: args.id});
+            }
+        },
+        author: {
+            type: DirectorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+            return _.find(directors, {id: args.id})
             }
         }
     }
