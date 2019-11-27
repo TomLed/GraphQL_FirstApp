3f06f8fed154a7049a8da7a4fdabf491bcf4470b
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-27 19:54:09 +0100

    Create React App and New Structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>The best ToDoList</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
