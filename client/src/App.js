4702b9b305e9f660870d23b4f3f54c4de89e24cf
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-27 20:00:09 +0100

    Add the Task List Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+//components
+import TaskList from './components/TaskList'
+
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>The best ToDoList</h1>
-
+          <TaskList />
       </div>
     );
   }
---
+++ client/src/components/TaskList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TaskList extends Component {
+    render() {
+        return (
+            <div>
+              <ul id="task-list">
+                  <li>Task name</li>
+              </ul>
+            </div>
+        );
+    }
+}
+
+export default TaskList;
