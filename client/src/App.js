53db0929bb2a7b9c09a39df5256f41df850696b7
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-27 20:07:57 +0100

    Setup Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
 //components
 import TaskList from './components/TaskList'
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>The best ToDoList</h1>
-          <TaskList />
-      </div>
+        <ApolloProvider client={client}>
+          <div id="main">
+            <h1>The best ToDoList</h1>
+            <TaskList />
+           </div>
+        </ApolloProvider>
     );
   }
 }
