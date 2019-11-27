7960e957604ea2eea2d1ef8dd7c25f9e224521f3
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-27 20:39:00 +0100

    Rendering data in components

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getTasksQuery = gql`
+    {
+        tasks {
+            name
+            id
+        }
+    }
+`;
 
 class TaskList extends Component {
+    displayTasks(){
+        var data = this.props.data;
+        if(data.loading){
+            return(<div>Loading tasks...</div>);
+        }else{
+            return data.tasks.map(task => {
+                return (
+                    <li key={task.id}>{task.name}</li>
+                );
+            })
+        }
+    }
     render() {
         return (
             <div>
               <ul id="task-list">
-                  <li>Task name</li>
+                  {this.displayTasks()}
               </ul>
             </div>
         );
     }
 }
 
-export default TaskList;
+export default graphql(getTasksQuery)(TaskList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require ('cors');
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect('mongodb://thomas:test123@ds037551.mlab.com:37551/gql-ecm');
