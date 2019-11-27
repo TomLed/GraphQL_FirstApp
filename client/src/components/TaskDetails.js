88c5b87a53c4139b2dc4c9821e5b7482efc60162
Thomas <thomas.ledar@centrale-marseille.fr>
2019-11-27 23:10:41 +0100

    Add function onCLick to have the details of the tasks when you click on it

--- client/src/components/TaskDetails.js
+++ client/src/components/TaskDetails.js
@@ -3,13 +3,42 @@ import {graphql} from 'react-apollo';
 import {getTaskQuery} from "../queries/queries";
 
 class TaskDetails extends Component {
+    displayTaskDetails(){
+        const { task } = this.props.data;
+        if(task){
+            return(
+                <div>
+                    <h2>{ task.name }</h2>
+                    <p>{ task.date }</p>
+                    <p>{ task.collaborator.name }</p>
+                    <p>All tasks by this collaborator:</p>
+                    <ul className="other-tasks">
+                        { task.collaborator.tasks.map(item => {
+                            return <li key={item.id}>{ item.name }</li>
+                        })}
+                    </ul>
+                </div>
+            );
+        } else {
+            return( <div>No book selected...</div> );
+        }
+    }
+
     render() {
         return(
             <div id="task-details">
-                <p>Output task details here</p>
+                { this.displayTaskDetails() }
             </div>
         );
     }
 }
 
-export default graphql(getTaskQuery)(TaskDetails);
+export default graphql(getTaskQuery, {
+    options: (props) => {
+        return {
+            variables: {
+                id: props.taskid
+            }
+        }
+    }
+})(TaskDetails);
